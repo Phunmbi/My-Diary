9a679364e07c248b16e38304339fbd9ba088c163
phunmbi <phunmbi@gmail.com>
2018-07-11 15:49:23 +0100

    Fix for signup page reload --#158976033

--- js/main.js
+++ js/main.js
@@ -1,12 +1,7 @@
 (() => {
     let submit = document.getElementById("submit");
     submit.addEventListener("click", (event) =>{
-        event.preventDefault(); 
-        if (location.pathname == "/index.html") {
-            location.assign("dashboard.html");
-        } else {
-            location.assign("index.html");
-        }
-    
+        event.preventDefault();     
+        location.assign("dashboard.html");
     })
-})()
+})();
