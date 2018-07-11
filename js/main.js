6b83bd96f7aa70f7377522e4aeb1b87e9b359732
phunmbi <phunmbi@gmail.com>
2018-07-11 15:20:20 +0100

    Fix page reload issue on landing page --#158976033

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
     let submit = document.getElementById("submit");
     submit.addEventListener("click", (event) =>{
         event.preventDefault(); 
-        if (location.pathname == "/UI/index.html") {
+        if (location.pathname == "/index.html") {
             location.assign("dashboard.html");
         } else {
             location.assign("index.html");
