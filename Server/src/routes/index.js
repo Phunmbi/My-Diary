7d7e35213b7dfa5f48d0b676f9a5c9956079df40
phunmbi <phunmbi@gmail.com>
2018-07-19 17:43:34 +0100

    Create API Endpoint to fetch one entry --#159070802

--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -10,6 +10,12 @@ router.get('/entries', (req, res) => {
   res.json(db.viewAll());
 });
 
+// get specific entry
+router.get('/entries/:id', (req, res) => {
+  const reply = db.viewOne(req.params.id);
+  res.json(reply);
+});
+
 // add an entry
 router.post('/entries', (req, res) => {
   db.addOne(req.body);
--- test/route.test.js
+++ test/route.test.js
@@ -8,38 +8,59 @@ let should = chai.should();
 
 chai.use(chaiHttp);
 //Our parent block
-describe('Entries', () => {
-    // Testing /POST 
-    describe('/POST entries', () => {
-        it('it should not POST a book without pages field', (done) => {
-            let entry = {
-                title: "Met a female dragon",
-                details: "had a fire conversation"
-            }
-            chai.request('http://localhost:3000/api/v1')
-                .post('/entries')
-                .send(entry)
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.be.a('object');
-                    res.body.should.have.property('message');
-                    done();
-                });
+describe("Entries", () => {
+  // Testing /POST
+  describe("/POST entries", () => {
+    it("it should not POST a book without pages field", done => {
+      let entry = {
+        title: "Met a female dragon",
+        details: "had a fire conversation"
+      };
+      chai
+        .request("http://localhost:3000/api/v1")
+        .post("/entries")
+        .send(entry)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a("object");
+          res.body.should.have.property("message");
+          done();
         });
     });
+  });
 
-    // Test the /GET route
-    describe('/GET database', () => {
-        it('it should GET all the entries', (done) => {
-            chai
-                .request("http://localhost:3000/api/v1")
-                .get("/entries")
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.be.a("array");
-                    res.body.length.should.be.eql(3);
-                    done();
-                });
+  // Test the /GET route
+  describe("/GET database", () => {
+    it("it should GET all the entries", done => {
+      chai
+        .request("http://localhost:3000/api/v1")
+        .get("/entries")
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a("array");
+          res.body.length.should.be.eql(3);
+          done();
         });
     });
+  });
+
+  // Test the /GET route
+  describe("/GET entry", () => {
+    it("it should GET a single entry", done => {
+      let entry = { title: "Met a bagel", details: "Asked if i had seen scones recently"};
+      let save = db.addOne(entry);
+      chai
+        .request("http://localhost:3000/api/v1")
+        .get("/entries/" + save[db.database.length - 1].id)
+        .send(entry)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a("object");
+          res.body.should.have.property('title');
+          res.body.should.have.property('details');
+          res.body.should.have.property('id');
+          done();
+        });
+    });
+  });
 });
