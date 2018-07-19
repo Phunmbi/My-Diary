eb540e9ef7cc3d1e5ff664da6d8403e272265f1c
phunmbi <phunmbi@gmail.com>
2018-07-19 11:33:41 +0100

    Add routes path

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,6 +1,7 @@
 import http from 'http';
 import express from 'express';
-// import bodyParser from 'body-parser';
+
+import routes from './routes/index';
 
 // Setup Server
 const app = express();
@@ -9,6 +10,7 @@ app.server = http.createServer(app);
 // Middleware
 
 // Api routes v1
+app.use('/api/v1/entries', routes);
 
 app.server.listen(process.env.port || 3000);
 console.log('started listening in on port 3000');
