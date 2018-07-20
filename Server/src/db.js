24c18aac7c027cf4b5c40605951b71a23436e08c
phunmbi <phunmbi@gmail.com>
2018-07-20 17:45:39 +0100

    Set up Heroku

--- Server/src/db.js
+++ Server/src/db.js
@@ -1,33 +1,35 @@
-const database = [];
+'use strict';
 
-const uniqueId = () => {
-  let id = '';
-  for (let index = 0; index < 7; index++) {
+var database = [];
+
+var uniqueId = () => {
+  var id = '';
+  for (var index = 0; index < 7; index++) {
     id += Math.round(Math.random() * 10);
   }
   return id;
 };
 
-const viewOne = (id) => {
-  for (let index = 0; index < database.length; index++) {
+var viewOne = id => {
+  for (var index = 0; index < database.length; index++) {
     if (database[index].id === id) {
       return database[index];
     }
   }
 };
 
-const addOne = (newEntry) => {
+var addOne = newEntry => {
   newEntry.id = uniqueId();
   database.push(newEntry);
   return database;
 };
 
-const viewAll = () => {
+var viewAll = () => {
   return database;
 };
 
-const modifyOne = (entry) => {
-  for (let index = 0; index < database.length; index++) {
+var modifyOne = entry => {
+  for (var index = 0; index < database.length; index++) {
     if (database[index].id === entry.id) {
       database.splice(index, 1, entry);
       return database[index];
@@ -35,8 +37,8 @@ const modifyOne = (entry) => {
   }
 };
 
-const deleteOne = (id) => {
-  for (let index = 0; index < database.length; index++) {
+var deleteOne = id => {
+  for (var index = 0; index < database.length; index++) {
     if (database[index].id === id) {
       database.splice(index, 1);
     }
--- Server/src/index.js
+++ Server/src/index.js
@@ -1,22 +1,39 @@
-import 'babel-polyfill';
-import http from 'http';
-import express from 'express';
-import bodyParser from 'body-parser';
+'use strict';
 
-import routes from './routes/index';
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _http = require('http');
+
+var _http2 = _interopRequireDefault(_http);
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+var _bodyParser = require('body-parser');
+
+var _bodyParser2 = _interopRequireDefault(_bodyParser);
+
+var _index = require('./routes/index');
+
+var _index2 = _interopRequireDefault(_index);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
 // Setup Server
-const app = express();
-app.server = http.createServer(app);
+var app = (0, _express2['default'])();
+app.server = _http2['default'].createServer(app);
 
 // Middleware
 // parse application/json
-app.use(bodyParser.json({}));
+app.use(_bodyParser2['default'].json({}));
 
 // Api routes v1
-app.use('/api/v1', routes);
+app.use('/api/v1', _index2['default']);
 
 app.server.listen(process.env.port || 3000);
 console.log('started listening in on port 3000');
 
-export default app;
+exports['default'] = app;
