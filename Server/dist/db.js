a0d0c7da9bac500603d20e3b532a01e44c24d568
phunmbi <phunmbi@gmail.com>
2018-07-20 18:05:41 +0100

    Setup heroku

---
+++ Server/dist/db.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var database = [];
+
+var uniqueId = () => {
+  var id = '';
+  for (var index = 0; index < 7; index++) {
+    id += Math.round(Math.random() * 10);
+  }
+  return id;
+};
+
+var viewOne = id => {
+  for (var index = 0; index < database.length; index++) {
+    if (database[index].id === id) {
+      return database[index];
+    }
+  }
+};
+
+var addOne = newEntry => {
+  newEntry.id = uniqueId();
+  database.push(newEntry);
+  return database;
+};
+
+var viewAll = () => {
+  return database;
+};
+
+var modifyOne = entry => {
+  for (var index = 0; index < database.length; index++) {
+    if (database[index].id === entry.id) {
+      database.splice(index, 1, entry);
+      return database[index];
+    }
+  }
+};
+
+var deleteOne = id => {
+  for (var index = 0; index < database.length; index++) {
+    if (database[index].id === id) {
+      database.splice(index, 1);
+    }
+  }
+  return database;
+};
+
+module.exports = {
+  database,
+  addOne,
+  viewOne,
+  viewAll,
+  modifyOne,
+  deleteOne
+};
---
+++ Server/dist/routes/index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _express = require('express');
+
+var _db = require('../db');
+
+var _db2 = _interopRequireDefault(_db);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var router = (0, _express.Router)();
+
+// api routes v1 (/v1)
+
+// /entries api, get all entries
+router.get('/entries', (req, res) => {
+  res.json(_db2['default'].viewAll());
+});
+
+// get specific entry
+router.get('/entries/:id', (req, res) => {
+  var reply = _db2['default'].viewOne(req.params.id);
+  res.json(reply);
+});
+
+// add an entry
+router.post('/entries', (req, res) => {
+  _db2['default'].addOne(req.body);
+  res.json({ message: 'New entry added' });
+});
+
+// edit an entry
+router.put('/entries/:id', (req, res) => {
+  req.body.id = req.params.id;
+  _db2['default'].modifyOne(req.body);
+  res.json({ message: 'Entry updated successfully' });
+});
+
+// delete an entry#
+router['delete']('/entries/:id', (req, res) => {
+  var entry = req.params.id;
+  _db2['default'].deleteOne(entry);
+  res.json({ message: 'This entry has been removed' });
+});
+
+exports['default'] = router;
