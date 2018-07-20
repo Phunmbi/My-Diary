577bbf4eacf2e234ce4cdc0948f5a73da7602588
phunmbi <phunmbi@gmail.com>
2018-07-20 18:45:20 +0100

    Setup Heroku

--- Server/src/index.js
+++ Server/src/index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json({}));
 // Api routes v1
 app.use('/api/v1', routes);
 
-app.server.listen(process.env.port || 3000);
+app.server.listen(process.env.PORT || 3000);
 console.log('started listening in on port 3000');
 
 export default app;
