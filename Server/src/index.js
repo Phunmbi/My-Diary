c1281ee9a4e9acb6044ce404b9b9dc63b3bb504c
phunmbi <phunmbi@gmail.com>
2018-07-20 19:23:37 +0100

    Setup Heroku

--- Server/src/index.js
+++ Server/src/index.js
@@ -4,18 +4,23 @@ import bodyParser from 'body-parser';
 
 import routes from './routes/index';
 
+// Specify port
+const port = process.env.PORT || 3000;
+
 // Setup Server
 const app = express();
 app.server = http.createServer(app);
 
 // Middleware
 // parse application/json
-app.use(bodyParser.json({}));
+app.use(bodyParser.json());
 
+app.get('/', (req, res) => res.json({
+  message: 'Welcome to MyDiary APIs'
+}));
 // Api routes v1
 app.use('/api/v1', routes);
 
-const port = process.env.PORT || 3000;
 app.server.listen(port, () => {
   console.log('started listening in on port 3000');
 });
