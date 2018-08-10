b2e35b21c07804247f6e3ed6a47aca0d4b070e86
phunmbi <phunmbi@gmail.com>
2018-08-10 15:16:38 +0100

    Feature(API): Implement get one entry functionality
    Use fetch to consume get one API endpoint
    change home icon
    change the structure of get all code, such that it saves input clicked
    on
    fixed text alignment to left
    
    [Delivers #159648743]

---
+++ client/js/getone.js
@@ -0,0 +1,32 @@
+window.addEventListener('load', () => {
+  const title = document.getElementById('title');
+  const date = document.getElementById('date');
+  const details = document.getElementById('details');
+  const errorResponse = document.getElementById('response');
+
+  const display = (entry) => {
+    title.innerText = entry.title;
+    date.innerText = entry.time_created.slice(0, 10);
+    details.innerText = entry.details;
+  };
+
+  fetch(`https://morning-falls-51849.herokuapp.com/api/v1/entries/${sessionStorage.getItem('entryId')}`, {
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json',
+      Authorization: `Bearer ${sessionStorage.getItem('token')}`
+    }
+  })
+    .then(resp => resp.json())
+    .then((data) => {
+      if (data.status === 200 || data.status === 200) {
+        console.log(data.data);
+        display(data.data);
+      } else {
+        errorResponse.innerText = data.message;
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
