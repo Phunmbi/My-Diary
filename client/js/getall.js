98011f5d3c2c60c41ada70a7de8da583926af7c1
phunmbi <phunmbi@gmail.com>
2018-08-10 17:35:22 +0100

    Feature(API): Consume Delete entry API
    Use fetch to consume delete API endpoint
    Create Modal to simulate delete confirmation box
    
    [Delivers #159648911]

--- client/js/getall.js
+++ client/js/getall.js
@@ -1,6 +1,9 @@
 window.addEventListener('load', () => {
-  const response = document.getElementById('response');
+  const errorResponse = document.getElementById('response');
   const section = document.getElementById('section');
+  const deleteModal = document.getElementById('deleteModal');
+  const noButton = document.getElementById('no-button');
+  const yesButton = document.getElementById('yes-button');
 
   const createCard = (data) => {
     // Create elements that make up a card
@@ -65,6 +68,37 @@ window.addEventListener('load', () => {
       sessionStorage.setItem('entryId', data.id);
       window.location.href = 'edit.html';
     });
+
+    deleteImg.addEventListener('click', () => {
+      sessionStorage.setItem('entryId', data.id);
+      deleteModal.style.display = 'block';
+    });
+
+    noButton.addEventListener('click', () => {
+      deleteModal.style.display = 'none';
+    });
+
+    yesButton.addEventListener('click', () => {
+      fetch(`https://morning-falls-51849.herokuapp.com/api/v1/entries/${sessionStorage.getItem('entryId')}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-type': 'application/json',
+          Authorization: `Bearer ${sessionStorage.getItem('token')}`
+        }
+      })
+        .then(resp => resp.json())
+        .then((response) => {
+          if (response.status === 200) {
+            window.location.href = 'dashboard.html';
+          } else {
+            console.log(response);
+            errorResponse.innerText = response.message;
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
   };
 
   const displayAll = (data) => {
@@ -88,15 +122,15 @@ window.addEventListener('load', () => {
         console.log(data);
         const name = sessionStorage.getItem('firstName');
         const cappedName = name.charAt(0).toUpperCase() + name.slice(1, name.length);
-        response.innerText = `Welcome to your Diary ${cappedName}`;
+        errorResponse.innerText = `Welcome to your Diary ${cappedName}`;
       } else if (data.status === 200) {
         const name = sessionStorage.getItem('firstName');
         const cappedName = name.charAt(0).toUpperCase() + name.slice(1, name.length);
-        response.innerText = `Welcome to your Diary ${cappedName}`;
+        errorResponse.innerText = `Welcome to your Diary ${cappedName}`;
         displayAll(data.data);
       } else {
         window.location.href = document.referrer;
-        response.innerText = data.message;
+        errorResponse.innerText = data.message;
       }
     })
     .catch((err) => {
