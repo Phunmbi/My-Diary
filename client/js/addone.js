2aa873658f678037bec0fb77a4d2b38a5b43bbe4
phunmbi <phunmbi@gmail.com>
2018-08-15 15:33:39 +0100

    Bug(Client): Protect pages from unauthorized access
    Move the add sign up a bit for mobile devices
    Protect add page, get one page, modify page and profile page
    Add home icon to the profile page
    
    [Delivers #159795312]

--- client/js/addone.js
+++ client/js/addone.js
@@ -1,45 +1,49 @@
-window.addEventListener('load', () => {
-  const submit = document.getElementById('submit');
+if (sessionStorage.getItem('token')) {
+  window.addEventListener('load', () => {
+    const submit = document.getElementById('submit');
 
-  // Attach an event listener on the submit button
-  submit.addEventListener('click', (event) => {
-    // Set up entry points
-    const title = document.getElementById('title').value;
-    const details = document.getElementById('details').value;
-    const errorResponse = document.getElementById('response');
+    // Attach an event listener on the submit button
+    submit.addEventListener('click', (event) => {
+      // Set up entry points
+      const title = document.getElementById('title').value;
+      const details = document.getElementById('details').value;
+      const errorResponse = document.getElementById('response');
 
-    submit.value = '';
-    submit.style.background = '#FEEF6D url(images/Spinner-1s.gif) no-repeat center';
+      submit.value = '';
+      submit.style.background = '#FEEF6D url(images/Spinner-1s.gif) no-repeat center';
 
-    const request = {
-      title,
-      details
-    };
+      const request = {
+        title,
+        details
+      };
 
-    fetch('https://morning-falls-51849.herokuapp.com/api/v1/entries', {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json',
-        Authorization: `Bearer ${sessionStorage.getItem('token')}`
-      },
-      body: JSON.stringify(request)
-    })
-      .then(resp => resp.json())
-      .then((data) => {
-        if (data.status === 200 || data.status === 201) {
-          console.log(request, data);
-          window.location.href = 'dashboard.html';
-          console.log('a');
-        } else {
-          console.log(data);
-          errorResponse.innerText = data.message;
-        }
+      fetch('https://morning-falls-51849.herokuapp.com/api/v1/entries', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+          Authorization: `Bearer ${sessionStorage.getItem('token')}`
+        },
+        body: JSON.stringify(request)
       })
-      .catch((err) => {
-        console.log(err);
-      });
+        .then(resp => resp.json())
+        .then((data) => {
+          if (data.status === 200 || data.status === 201) {
+            console.log(request, data);
+            window.location.href = 'dashboard.html';
+            console.log('a');
+          } else {
+            console.log(data);
+            errorResponse.innerText = data.message;
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
 
-    // Prevent Default
-    event.preventDefault();
+      // Prevent Default
+      event.preventDefault();
+    });
   });
-});
+} else {
+  window.location.href = 'index.html';
+}
--- client/js/profile.js
+++ client/js/profile.js
@@ -1,13 +1,17 @@
-window.addEventListener('load', () => {
-  const signOut = document.getElementById('signOut');
-  const entriesNumber = document.getElementById('entries');
+if (sessionStorage.getItem('token')) {
+  window.addEventListener('load', () => {
+    const signOut = document.getElementById('signOut');
+    const entriesNumber = document.getElementById('entries');
 
-  entriesNumber.innerHTML = `You have a total of ${sessionStorage.getItem('entriesTotal')} entries`;
+    entriesNumber.innerHTML = `You have a total of ${sessionStorage.getItem('entriesTotal')} entries`;
 
-  signOut.addEventListener('click', () => {
-    signOut.value = '';
-    signOut.style.background = 'rgb(20, 43, 68) url(images/Spinner-1s.gif) no-repeat center';
-    sessionStorage.clear();
-    window.location.href = 'index.html';
+    signOut.addEventListener('click', () => {
+      signOut.value = '';
+      signOut.style.background = 'rgb(20, 43, 68) url(images/Spinner-1s.gif) no-repeat center';
+      sessionStorage.clear();
+      window.location.href = 'index.html';
+    });
   });
-});
+} else {
+  window.location.href = 'index.html';
+}
